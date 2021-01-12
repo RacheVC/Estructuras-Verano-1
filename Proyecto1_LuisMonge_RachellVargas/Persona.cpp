4f5d6cd4907e3b164ca921246593dd39575c2331
luismc1615 <58350003+luismc1615@users.noreply.github.com>
2021-01-12 01:02:48 -0600

    Clase Persona creada

--- Proyecto1_LuisMonge_RachellVargas/Persona.cpp
+++ Proyecto1_LuisMonge_RachellVargas/Persona.cpp
@@ -3,3 +3,60 @@
 //
 
 #include "Persona.h"
+
+Persona::Persona() {
+    nombre = "indefinido";
+    id = "indefinido";
+    tieneHijos = "indefinido";
+    estaEmbarazada = "indifinido";
+    esAdultoMayor = "indefinido";
+    categoria = "indefinida"
+}
+
+void Persona::setNombre(string &nombre) {
+    Persona::nombre = nombre;
+}
+
+void Persona::setId(string &id) {
+    Persona::id = id;
+}
+
+void Persona::setTieneHijos(string &tieneHijos) {
+    Persona::tieneHijos = tieneHijos;
+}
+
+void Persona::setEstaEmbarazada(string &estaEmbarazada) {
+    Persona::estaEmbarazada = estaEmbarazada;
+}
+
+void Persona::setEsAdultoMayor(string &esAdultoMayor) {
+    Persona::esAdultoMayor = esAdultoMayor;
+}
+
+void Persona::setCategoria(string &categoria) {
+    Persona::categoria = categoria;
+}
+
+string &Persona::getNombre() {
+    return nombre;
+}
+
+string &Persona::getId() {
+    return id;
+}
+
+string &Persona::getTieneHijos() {
+    return tieneHijos;
+}
+
+string &Persona::getEstaEmbarazada() {
+    return estaEmbarazada;
+}
+
+string &Persona::getEsAdultoMayor() {
+    return esAdultoMayor;
+}
+
+string &Persona::getCategoria() {
+    return categoria;
+}
--- Proyecto1_LuisMonge_RachellVargas/Persona.h
+++ Proyecto1_LuisMonge_RachellVargas/Persona.h
@@ -4,11 +4,45 @@
 
 #ifndef PROYECTO1_LUISMONGE_RACHELLVARGAS_PERSONA_H
 #define PROYECTO1_LUISMONGE_RACHELLVARGAS_PERSONA_H
+#include <iostream>
 
+using namespace std;
 
 class Persona {
-private:
+public:
+    Persona();
 
+    string &getNombre();
+
+    string &getId();
+
+    string &getTieneHijos();
+
+    string &getEstaEmbarazada();
+
+    string &getEsAdultoMayor();
+
+    string &getCategoria();
+
+    void setNombre(string &nombre);
+
+    void setId(string &id);
+
+    void setTieneHijos(string &tieneHijos);
+
+    void setEstaEmbarazada(string &estaEmbarazada);
+
+    void setEsAdultoMayor(string &esAdultoMayor);
+
+    void setCategoria(string &categoria);
+
+private:
+    string nombre;
+    string id;
+    string tieneHijos;
+    string estaEmbarazada;
+    string esAdultoMayor;
+    string categoria;
 };
 
 

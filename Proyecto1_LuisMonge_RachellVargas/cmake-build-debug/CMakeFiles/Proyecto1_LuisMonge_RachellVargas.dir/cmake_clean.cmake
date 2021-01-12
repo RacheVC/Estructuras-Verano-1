8dae882c70c9bfd217f033ab67053da06e212787
RacheVC <69211990+RacheVC@users.noreply.github.com>
2021-01-11 21:55:37 -0600

    Carpeta y creacion del proyecto

---
+++ Proyecto1_LuisMonge_RachellVargas/cmake-build-debug/CMakeFiles/Proyecto1_LuisMonge_RachellVargas.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proyecto1_LuisMonge_RachellVargas.dir/main.cpp.obj"
+  "Proyecto1_LuisMonge_RachellVargas.exe"
+  "Proyecto1_LuisMonge_RachellVargas.exe.manifest"
+  "Proyecto1_LuisMonge_RachellVargas.pdb"
+  "libProyecto1_LuisMonge_RachellVargas.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proyecto1_LuisMonge_RachellVargas.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
